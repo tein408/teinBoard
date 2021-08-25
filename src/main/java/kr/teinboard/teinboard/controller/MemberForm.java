96068d99f16a8de8151549da09bbadf7cbc57e23
tein408 <tein408@gmail.com>
2021-08-25 21:34:16 +0900

    feat: 회원 가입 정보 전달할 class 추가

---
+++ src/main/java/kr/teinboard/teinboard/controller/MemberForm.java
@@ -0,0 +1,32 @@
+package kr.teinboard.teinboard.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String pwd;
+
+    private String auth;
+
+    public MemberForm() {
+    }
+
+    public MemberForm(String name, String email, String pwd, String auth) {
+        this.name = name;
+        this.email = email;
+        this.pwd = pwd;
+        this.auth = auth;
+    }
+
+}
