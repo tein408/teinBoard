c1ef700cea72796f9fba171fad3c5508815a3159
tein408 <tein408@gmail.com>
2021-08-25 21:51:38 +0900

    feat: 회원가입 처리

---
+++ src/main/java/kr/teinboard/teinboard/repository/MemberRepository.java
@@ -0,0 +1,19 @@
+package kr.teinboard.teinboard.repository;
+
+import kr.teinboard.teinboard.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+}
