d17257f8fe7adb2c2d79019e94c20c384dc3657c
tein408 <tein408@gmail.com>
2021-08-25 21:51:23 +0900

    feat: 회원가입 post 요청 처리

--- src/main/java/kr/teinboard/teinboard/controller/MemberController.java
+++ src/main/java/kr/teinboard/teinboard/controller/MemberController.java
@@ -1,16 +1,35 @@
 package kr.teinboard.teinboard.controller;
 
+import kr.teinboard.teinboard.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+
+    private final MemberService memberService;
 
     @GetMapping(value = "/members/new")
     public String createMemberForm(Model model) {
         model.addAttribute("memberForm", new MemberForm());
         return "/members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String createMember(@Valid MemberForm memberForm, BindingResult result) {
+        //BindingResult -> name field에 대해 에러를 뽑아준다.
+        if(result.hasErrors()){
+            return "/members/createMemberForm";
+        }
+        memberService.saveMember(memberForm);
+        return "/members/signUp";
     }
 
 }
