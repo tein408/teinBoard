e0128a06633f74bb71fb6b5ba18e552ce1738da9
tein408 <tein408@gmail.com>
2021-08-25 21:13:05 +0900

    feat: Initial setting

---
+++ src/main/java/kr/teinboard/teinboard/TeinBoardApplication.java
@@ -0,0 +1,13 @@
+package kr.teinboard.teinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeinBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeinBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/teinboard/teinboard/TeinBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.teinboard.teinboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeinBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
